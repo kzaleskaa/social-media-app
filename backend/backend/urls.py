fe9fbcaa15f48908c8d70b1703814c3905c45d43
Kasia Zaleska <kzaleska416@gmail.com>
2022-03-28 15:23:46 +0200

    add create_superuser function

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,12 +1,11 @@
 from django.contrib import admin
-from django.urls import include, path, re_path
+from django.urls import include, path
 from django.views.generic import TemplateView
 
 urlpatterns = [
-    re_path('auth/', include('djoser.urls')),
-    re_path('auth/', include('djoser.urls.jwt')),
-    # path('admin/', admin.site.urls),
-    # path('', include('users.urls')),
+    path('admin/', admin.site.urls),
+    path('auth/', include('djoser.urls')),
+    path('auth/', include('djoser.urls.jwt')),
 ]
 
-urlpatterns += [re_path(r'^.*', TemplateView.as_view(template_name='index.html'))]
+# urlpatterns += [re_path(r'^.*', TemplateView.as_view(template_name='index.html'))]
--- backend/users/models.py
+++ backend/users/models.py
@@ -3,15 +3,28 @@ from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseU
 
 
 class UserAccountManager(BaseUserManager):
-    def create_user(self, email, first_name, last_name, password=None):
+    def create_user(self, email, password=None, **extra_fields):
+        """
+        Creates and saves a User with the given data.
+        """
         if not email:
             raise ValueError('Users must have an email address')
 
-        email = self.normalize_email(email)
-        user = self.model(email=email, first_name=first_name, last_name=last_name)
+        user = self.model(email=self.normalize_email(email), **extra_fields)
 
         user.set_password(password)
         user.save()
+
+        return user
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        """
+        Creates and saves a superuser with the given email and password.
+        """
+        extra_fields.setdefault('is_active', True)
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        user = self.create_user(email, password=password, **extra_fields)
 
         return user
 
