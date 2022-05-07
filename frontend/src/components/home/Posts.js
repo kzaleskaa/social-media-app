d33456aa84f7a9e37d129d5a805859a5b8f7e170
Kasia Zaleska <kzaleska416@gmail.com>
2022-05-07 16:03:22 +0200

    create custom http hook

--- frontend/src/components/home/Posts.js
+++ frontend/src/components/home/Posts.js
@@ -1,59 +1,52 @@
-import axios from "axios";
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import classes from "./Posts.module.css";
+import useHttp from "../../hooks/use-http";
 
 const Posts = () => {
-  const [posts, setPosts] = useState([]);
+  const [posts, setPosts] = useState();
 
-  const downloadPosts = async () => {
-    const configuration = {
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${localStorage.getItem("access")}`,
-      },
-    };
-    try {
-      const result = await axios.get(
-        `${process.env.REACT_APP_BACKEND}/api/posts/home`,
-        configuration
-      );
-
-      setPosts(result.data.posts);
-    } catch (err) {
-      alert("Something went wrong! Try again!");
+  const httpData = useHttp(
+    {
+      url: `${process.env.REACT_APP_BACKEND}/api/posts/home`,
+    },
+    (response) => {
+      setPosts(response.posts);
     }
-  };
+  );
+
+  const { isLoading, error, sendRequest: downloadPosts } = httpData;
 
   useEffect(() => {
     downloadPosts();
   }, []);
 
-  console.log(posts);
-
   return (
     <div className={classes["posts-container"]}>
-      {posts.map((item, index) => (
-        <div key={index} className={classes.post}>
-          <div className={classes.info}>
-            <div className={classes.profile}>
+      {posts &&
+        posts.map((item, index) => (
+          <div key={index} className={classes.post}>
+            <div className={classes.info}>
+              <div className={classes.profile}>
+                <img
+                  src={`${process.env.REACT_APP_BACKEND}${item.user.image}`}
+                  alt="user profile"
+                  className={classes["profile-image"]}
+                />
+              </div>
+              <Link to={`/profile/${item.user.nickname}`}>
+                {item.user.nickname}
+              </Link>
+            </div>
+            <div className={classes["post-container"]}>
               <img
-                src={`${process.env.REACT_APP_BACKEND}${item.user.image}`}
-                alt="user profile"
-                className={classes["profile-image"]}
+                src={`${process.env.REACT_APP_BACKEND}${item.image}`}
+                className={classes["post-image"]}
+                alt="user's post"
               />
             </div>
-            <Link to={`/profile/${item.user.nickname}`}>
-              {item.user.nickname}
-            </Link>
           </div>
-            <img
-              src={`${process.env.REACT_APP_BACKEND}${item.image}`}
-              className={classes["post-image"]}
-              alt="user's post"
-            />
-        </div>
-      ))}
+        ))}
     </div>
   );
 };
---
+++ frontend/src/hooks/use-http.js
@@ -0,0 +1,35 @@
+import axios from "axios";
+import { useState } from "react";
+
+const defaultHeader = {
+  headers: {
+    "Content-Type": "application/json",
+    Authorization: `Bearer ${localStorage.getItem("access")}`,
+  },
+};
+
+const useHttp = (
+  { url, method = "get", body = null, header = defaultHeader },
+  applyData
+) => {
+  const [error, setError] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const sendRequest = async () => {
+    setError(false);
+    setIsLoading(true);
+
+    try {
+      const response = await axios[method](url, header, body);
+      applyData(response.data);
+    } catch (err) {
+      setError(err.msg || "Something went wrong! Try again!");
+    }
+
+    setIsLoading(false);
+  };
+
+  return { isLoading, error, sendRequest };
+};
+
+export default useHttp;
